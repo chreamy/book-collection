de0eef50ba9715a8bcb3e4fc13a366bd627c7d90
root <root@localhost>
2025-09-09 22:53:32 +0000

    book collection 2

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,15 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :published_date, presence: { message: "can't be blank" }
+  validate :published_date_cannot_be_in_future
+
+  private
+
+  def published_date_cannot_be_in_future
+    if published_date.present? && published_date > Date.today
+      errors.add(:published_date, "can't be in the future")
+    end
+  end
 end
