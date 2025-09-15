2506b7cbf9b761e1afb86b89f9262f39b7eeec41
Christopher Liu <116124688+chreamy@users.noreply.github.com>
2025-09-15 17:36:51 -0500

    heroku

--- babel.config.js
+++ babel.config.js
@@ -41,18 +41,7 @@ module.exports = function (api) {
             "@babel/plugin-syntax-dynamic-import",
             isTestEnv && "babel-plugin-dynamic-import-node",
             "@babel/plugin-transform-destructuring",
-            [
-                "@babel/plugin-proposal-class-properties",
-                {
-                    loose: true,
-                },
-            ],
-            [
-                "@babel/plugin-proposal-object-rest-spread",
-                {
-                    useBuiltIns: true,
-                },
-            ],
+
             [
                 "@babel/plugin-transform-runtime",
                 {
